141b870103c3ba50089f8537ac264b0717e6376c
timeobserver <timeobserver@windows10.microdone.cn>
2016-05-18 21:38:31 +0800

    test

---
+++ src/com/struts/StrutsTest.java
@@ -0,0 +1,16 @@
+package com.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StrutsTest extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {	
+		System.out.println("Ö´ÐÐAction");
+		return SUCCESS;
+	}
+	
+	
+	
+
+}
